3b647dbd571a25ad0dd5e3a18da9cdffc1fc9820
rashu-jain <59656407+rashu-jain@users.noreply.github.com>
2020-06-25 22:26:51 +0530

    Add files via upload

--- foodpjt/js/toptiffin.js
+++ foodpjt/js/toptiffin.js
@@ -1,15 +1,8 @@
 var vendorList = document.getElementById("vendors");
  
-  db.collection('tiffen_service_details').get().then(function(querySnapshot) {
+  db.collection('tiffen_service_details').limit(5).get().then(function(querySnapshot) {
       querySnapshot.forEach(function(doc) {
-        //tiffen_service_detailsRef.orderBy("totalCost").limit(5)
-        var img = document.createElement("img");
-        img.src = doc.data()["Logo Image"];
-        img.classList.add("listimage");
-
-        vendorList.innerHTML +=   "<tr><td>"
-        document.getElementById("vendors").appendChild(img);
-
+        
         vendorList.innerHTML += "</td><td>"+doc.data().OwnerName+"</td><td>"+doc.data().Email+
         "</td><td>"+doc.data().Phone+
         "</td><td class='text-center'></tr>"
