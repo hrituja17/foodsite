01b55757e6ffb3a130ebcc7b5c5b065a28abcdaf
rashu-jain <59656407+rashu-jain@users.noreply.github.com>
2020-06-25 07:54:38 +0530

    Add files via upload

--- foodpro/js/index.js
+++ foodpro/js/index.js
@@ -1,4 +1,60 @@
 const db = firebase.firestore();
+var totalearning = 0;
+var total =0;
+var orders=0;
+var vendors =0;
+var users = 0;
+var vendor=0;
+   
+   db.collection("tiffen_service_details/saibhavadeesh@gmail.com/acceptedOrders").get().then(function(querySnapshot) {
+
+       querySnapshot.forEach(function(doc) {
+        totalearning += doc.data().totalCost;
+        });
+       earning.innerHTML += "<h3>" + totalearning + "</h3>";
+    });
+
+
+  
+    db.collection("tiffen_service_details/saibhavadeesh@gmail.com/acceptedOrders").get().then(function(querySnapshot) {
+    
+        querySnapshot.forEach(function(doc) {
+            total += doc.data().totalCost;
+        });
+        earnings.innerHTML += "<h3>" + total + "</h3>";
+    });
+
+    
+    
+    db.collection("vendor_collection/vendors/registered_vendors").get().then(function(querySnapshot){
+        querySnapshot.forEach(function(doc){
+            vendors ++;
+        });
+        totalvendors.innerHTML += "<h3>" + vendors + "</h3>";
+    });
+
+
+   db.collection("customer_collection").get().then(function(querySnapshot){
+        querySnapshot.forEach(function(doc){
+            users++;
+         });
+         totalusers.innerHTML += "<h3>" + users + "</h3>";
+   });
+
+   db.collection("tiffen_service_details/saibhavadeesh@gmail.com/acceptedOrders").get().then(function(querySnapshot){
+        querySnapshot.forEach(function(doc){
+            orders++;
+          });
+        totalorders.innerHTML += "<h3>" + orders + "</h3>";
+    });
+    
+    db.collection("vendor_collection/vendors/registered_vendors").get().then(function(querySnapshot){
+         querySnapshot.forEach(function(doc){
+           vendor ++;
+        });
+         totalvendor.innerHTML += "<h3>" + vendor + "</h3>";
+    });
+
 
 
 function getuser(){
@@ -78,115 +134,8 @@ db.collection('vendor_collection/vendors/registered_vendors/').get().then(functi
 
 
 
-var totalearning = 0;
-var total =0;
-var orders=0;
-var vendors =0;
-var users = 0;
-var vendor=0;
-   
-   db.collection("tiffen_service_details/saibhavadeesh@gmail.com/acceptedOrders").get().then(function(querySnapshot) {
-
-       querySnapshot.forEach(function(doc) {
-        totalearning += doc.data().totalCost;
-        });
-       earning.innerHTML += "<h3>" + totalearning + "</h3>";
-    });
-
-
-  
-    db.collection("tiffen_service_details/saibhavadeesh@gmail.com/acceptedOrders").get().then(function(querySnapshot) {
-    
-        querySnapshot.forEach(function(doc) {
-            total += doc.data().totalCost;
-        });
-        earnings.innerHTML += "<h3>" + total + "</h3>";
-    });
-
-    
-    
-    db.collection("vendor_collection/vendors/registered_vendors").get().then(function(querySnapshot){
-        querySnapshot.forEach(function(doc){
-            vendors ++;
-        });
-        totalvendors.innerHTML += "<h3>" + vendors + "</h3>";
-    });
-
-
-   db.collection("customer_collection").get().then(function(querySnapshot){
-        querySnapshot.forEach(function(doc){
-            users++;
-         });
-         totalusers.innerHTML += "<h3>" + users + "</h3>";
-   });
-
-   db.collection("tiffen_service_details/saibhavadeesh@gmail.com/acceptedOrders").get().then(function(querySnapshot){
-        querySnapshot.forEach(function(doc){
-            orders++;
-          });
-        totalorders.innerHTML += "<h3>" + orders + "</h3>";
-    });
-    
-    db.collection("vendor_collection/vendors/registered_vendors").get().then(function(querySnapshot){
-         querySnapshot.forEach(function(doc){
-           vendor ++;
-        });
-         totalvendor.innerHTML += "<h3>" + vendor + "</h3>";
-    });
-
-
-// where(firebase.firestore.FieldPath.documentId(), '==', 'saibhavadeesh@gmail.com')
 
 
 
-// TEST CODE
 
-/*
-var total = 0;
 
-db.collection("tiffen_service_details/saibhavadeesh@gmail.com/acceptedOrders").get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc){
-        total += doc.data().totalCost;
-    });
-    earnings.innerHTML = "<h3>" + total + "</h3>";
-});
-
-*/
-
-/* Auth code 
-
-var notLoggedIn = document.getElementById("notLoggedIn");
-var loggedIn = document.getElementById("loggedIn");
-
-firebase.auth().onAuthStateChanged(function(user) {
-    if(user) {
-        notLoggedIn.style.display = "none";
-        loggedIn.style.display = "block";
-    } else {
-        notLoggedIn.style.display = "block";
-        loggedIn.style.display = "none";
-    }
-});
-
-function login() {
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-
-        window.alert("Error: " + errorMessage);
-    });
-}
-
-function logout() {
-    firebase.auth().signOut().then(function() {
-        window.location.href = "http://127.0.0.1:5500/index.html"
-    })
-    .catch(function(error) {
-        window.alert("Error:" + error);
-    });
-}
-
-*/
