4392dde37e32444b36680a1e6ca9cb65a8fa7677
akanksha2508 <51355448+akanksha2508@users.noreply.github.com>
2020-06-25 21:53:25 +0530

    Add files via upload

--- food/js/notification.js
+++ food/js/notification.js
@@ -1,10 +1,55 @@
 var db = firebase.firestore();
 var noti = document.getElementById("notify");
+
+
 db.collection('subscription_notification').get().then(function(querySnapshot) {
     querySnapshot.forEach(function(doc) {
-        if(doc.data().status==='pending'){
-      noti.innerHTML += "<a data-toggle='modal' data-target='#notModal'>"+doc.data().vendorEmail+"</a>"
+        if(doc.data().status === "pending"){
+          var li = document.createElement("li");
+          li.classList.add("list-group-item");
+        li.innerHTML += "<a data-toggle='modal' data-target='#notModal'><b>You got a notification for</b> "+doc.data().vendorEmail+"</a>";
+        noti.appendChild(li);
+        var docId = doc.id;
+          db.collection('subscription_notification').doc(docId).get().then(function(oneDoc){
+              var img = document.createElement("img");
+              var name=document.createElement("name");
+              name.innerHTML +="<center>"+oneDoc.data().vendorEmail+"</center>";
+              img.src = oneDoc.data().proofOfPayment;
+              img.classList.add("listimage");
+              document.getElementById("email").appendChild(name);
+              document.getElementById("pimg").appendChild(img);
+              
+          });
         }
     });
-  
 });
+
+function activate(){
+
+var email=document.getElementById("email").value;
+db.collection('subscription_notification').get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+        if(doc.data().vendorEmail === email){
+            var docId = doc.id;
+    db.collection('subscription_notification').doc(docId).update({ status: "active" }).then(function(){
+    window.alert("Update successful");
+})
+.catch(err => window.alert("Error: " + err.message));
+        }
+    });
+});
+}
+function reject(){
+    var email=document.getElementById("email").value;
+    db.collection('subscription_notification').get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            if(doc.data().vendorEmail === email){
+                var docId = doc.id;
+        db.collection('subscription_notification').doc(docId).update({ status: "reject" }).then(function(){
+        window.alert("Update successful");
+    })
+    .catch(err => window.alert("Error: " + err.message));
+            }
+        });
+    });
+}
