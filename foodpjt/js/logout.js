0eed5ef4cdc8d34d14331f3e349c20b47878ec02
hrituja17 <55551450+hrituja17@users.noreply.github.com>
2020-06-25 16:15:28 +0530

    updated logout.js

--- foodpjt/js/logout.js
+++ foodpjt/js/logout.js
@@ -1,6 +1,13 @@
+firebase.auth().onAuthStateChanged(function(user) {
+    if(!user) {
+        window.location.href="../public/index.html";
+    } 
+});
+
+
 function logout() {
     firebase.auth().signOut().then(function() {
-        window.location.href = "../public/logout.html"
+        window.location.href = "../public/logout.html";
     })
     .catch(function(error) {
         window.alert("Error:" + error);
